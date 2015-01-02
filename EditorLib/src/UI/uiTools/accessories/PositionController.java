50e269aa9d1e95d3e1758d32375e7361c93ec761
dustin <misterdustinface@gmail.com>
2015-01-02 14:36:34 -0800

    Minor modifications

--- EditorLib/src/UI/uiTools/accessories/PositionController.java
+++ EditorLib/src/UI/uiTools/accessories/PositionController.java
@@ -3,9 +3,25 @@ package UI.uiTools.accessories;
 import shapes.Point;
 
 public class PositionController {
-	public Point position;
-	public PositionController(){position = new Point(0,0);}
-	public void reset(){position.set(0, 0);}
-	public Point getPosition(){return position;}
+	public  Point position;
+	private Point initialPosition;
+
+	public PositionController() {
+		initialPosition = new Point(0,0);
+		position = new Point(0,0);
+	}
+	
+	public PositionController(Point INITIAL_POSITION) {
+		initialPosition = new Point(INITIAL_POSITION.x, INITIAL_POSITION.y);
+		position = new Point(INITIAL_POSITION.x, INITIAL_POSITION.y);
+	}
+	
+	public void reset() {
+		position.set(initialPosition.x, initialPosition.y);
+	}
+	
+	public Point getPosition() {
+		return position;
+	}
 	
 }
--- EditorLib/src/UI/uiTools/accessories/ScaleController.java
+++ EditorLib/src/UI/uiTools/accessories/ScaleController.java
@@ -2,8 +2,17 @@ package UI.uiTools.accessories;
 
 public class ScaleController{
 	public float scale;
-	public ScaleController(){scale = 1f;}
-	public void reset(){scale = 1f;}
-	public float getScale(){return scale;};
+	
+	public ScaleController() {
+		scale = 1f;
+	}
+	
+	public void reset() {
+		scale = 1f;
+	}
+	
+	public float getScale() {
+		return scale;
+	};
 	
 }
