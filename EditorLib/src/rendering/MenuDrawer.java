dc62bff7d1f8de7dfb71940834aa2fd160f4d4dc
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2015-03-24 20:55:50 -0700

    Added TextLabel to MenuDrawer

--- EditorLib/src/rendering/MenuDrawer.java
+++ EditorLib/src/rendering/MenuDrawer.java
@@ -4,15 +4,17 @@ import shapes.Point;
 import UI.widgets.BarSlider;
 import UI.widgets.FileChooser;
 import UI.widgets.MenuButton;
+import UI.widgets.TextLabel;
 import UI.widgets.UIMenu;
 
 public interface MenuDrawer {
+	void drawMenuBox         	( Point topLeft, int width, int height);
+	void drawMenuBox			( int X, int Y, int width, int height);
 	void drawButton       		( MenuButton b );
 	void drawSlider       		( BarSlider s );
 	void drawSelectorArrow		( MenuButton b, int x, int size );
-	//void drawPlusOnButton 		( MenuButton b );
-	void drawMenuBox         	( Point topLeft, int width, int height);
-	void drawMenuBox			( int X, int Y, int width, int height);
+	void drawPlusOnButton 		( MenuButton b );
+	void drawTextLabel          ( TextLabel tl);
 	void drawUIMenu       		( UIMenu menu );
 	void drawFileChooser		( FileChooser fileChooser );
 }
