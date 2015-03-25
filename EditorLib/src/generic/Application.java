390c1f1b3507074dbc99fb76cb35e9eafe61634a
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2015-03-24 22:12:42 -0700

    - Modified Whitespace - Modified Application class

--- EditorLib/src/generic/Application.java
+++ EditorLib/src/generic/Application.java
@@ -1,74 +1,43 @@
 package generic;
 
+import generic.datastructures.Table;
+
 import java.util.LinkedList;
 
 public class Application {
 	
-	private Thread mainThread; 
-	private Thread renderThread;
-	private Thread drawerThread;
-	private Thread audioSystemThread;
-	private Thread networkThread;
-	
-	private LinkedList<Thread> existingThreads;
+	private Table<Thread> threads;
+	private LinkedList<Thread> threadsList;
 	
 	public Application() {
-		existingThreads = new LinkedList<Thread>();
+		threads = new Table<Thread>();
+		threadsList = new LinkedList<Thread>();
 	}
 	
 	public void setMain(TickingLoop PROGRAM_MAIN) {
-		mainThread = new Thread(PROGRAM_MAIN);
-		mainThread.setName("MAIN");
-		mainThread.setPriority(Thread.MAX_PRIORITY);
-		existingThreads.add(mainThread);
+		addComponent("MAIN", PROGRAM_MAIN);
+		threads.get("MAIN").setPriority(Thread.MAX_PRIORITY);
 	}
 	
-	public void setRenderer(Runnable PROGRAM_RENDERER) {
-		renderThread = new Thread(PROGRAM_RENDERER);
-		renderThread.setName("RENDERER");
-	
-		existingThreads.add(renderThread);
-	}
-	
-	public void setDrawer(Runnable DRAWER) {
-		drawerThread = new Thread(DRAWER);
-		drawerThread.setName("DRAWER");
-		
-		existingThreads.add(drawerThread);
-	}
-	
-	public void setAudioSystem(Runnable PROGRAM_AUDIO_SYSTEM) {
-		audioSystemThread = new Thread(PROGRAM_AUDIO_SYSTEM);
-		audioSystemThread.setName("AUDIO_SYS");
-		
-		existingThreads.add(audioSystemThread);
-	}
-	
-	public void setNetworkThread(Runnable NETWORK_THREAD) {
-		networkThread = new Thread(NETWORK_THREAD);
-		networkThread.setName("NETWORK");
-		
-		existingThreads.add(networkThread);
+	public void addComponent(String name, Runnable runnableComponent) {
+		Thread newThread = new Thread(runnableComponent);
+		newThread.setName(name);
+		threads.insert(name, newThread);
+		threadsList.add(newThread);
 	}
 	
 	public void start() {
-		for (Thread thread : existingThreads) {
+		for (Thread thread : threadsList) {
 			thread.start();
 		}
 	}
 	
-	public void startAudio() {
-		startThread(audioSystemThread);
-	}
-	public void stopAudio() {
-		stopThread(audioSystemThread);
+	public void startComponent(String name) {
+		startThread(threads.get(name));
 	}
 	
-	public void startNetwork() {
-		startThread(networkThread);
-	}
-	public void stopNetwork() {
-		stopThread(networkThread);
+	public void stopComponent(String name) {
+		stopThread(threads.get(name));
 	}
 	
 	private void startThread(Thread thread) {
