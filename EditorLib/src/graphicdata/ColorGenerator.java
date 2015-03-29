d7f7797c9529cfebe6976b0aeb4dc9b81b8b27e2
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2015-03-29 11:43:14 -0700

    - Created Property-based data structures which follow the "Named Data" paradigm. - Added generic GraphicData and ColorGenerator objects.

---
+++ EditorLib/src/graphicdata/ColorGenerator.java
@@ -0,0 +1,43 @@
+package graphicdata;
+
+import generic.structures.ColorData;
+import generic.tags.Library;
+
+import java.util.Random;
+
+final public class ColorGenerator implements Library {
+	
+	private ColorGenerator () {
+		
+	}
+	
+	public static ColorData randomColor() {
+		return new ColorData((float)Math.random(), (float)Math.random(), (float)Math.random(), 0.0f);
+	}
+	
+	//http://stackoverflow.com/questions/43044/algorithm-to-randomly-generate-an-aesthetically-pleasing-color-palette
+	public static ColorData randomPastel() {
+		return new ColorData((float)Math.random()/2, (float)Math.random()/2, (float)Math.random()/2, 0.0f);
+	}
+	
+	//http://stackoverflow.com/questions/43044/algorithm-to-randomly-generate-an-aesthetically-pleasing-color-palette
+	public static ColorData mixColor(ColorData mix) {
+	    Random random = new Random();
+	    float red   = (int) ((random.nextFloat() + mix.r) / 2);
+	    float green = (int) ((random.nextFloat() + mix.g) / 2);
+	    float blue  = (int) ((random.nextFloat() + mix.b) / 2);
+	    return new ColorData(red, green, blue, 0);
+	}
+	
+	public static ColorData getCompliment(ColorData primary) {
+		return new ColorData(1 - primary.r, 1 - primary.g, 1 - primary.b, 0);
+	}
+	
+	public static ColorData getIntermediateColor(ColorData primary, ColorData secondary) {
+		return new ColorData((primary.r + secondary.r)/2,
+							 (primary.g + secondary.g)/2,
+							 (primary.b + secondary.b)/2,
+							 (primary.a + secondary.a)/2 );
+	}
+	
+}
---
+++ EditorLib/src/graphicdata/GraphicData.java
@@ -0,0 +1,12 @@
+package graphicdata;
+
+import generic.datastructures.Properties;
+import generic.tags.NamedData;
+
+public abstract class GraphicData extends Properties implements NamedData {
+
+	protected GraphicData() {
+
+	}
+	
+}
